bd3b281a0609fb09ca8947d898ae296bef588598
Bryan Crompton <bryan.crompton@gmail.com>
2021-10-23 15:10:09 -0400

    $(date)

--- push-rss.py
+++ push-rss.py
@@ -6,11 +6,15 @@ import os
 
 num = random.randint(0, 1000000)
 
+f = open("file:///home/bryan/tidy/myweb/bookmarks/article-html/d00148e4314a87df.html.art.html")
+description = f.read()
+f.close()
+
 # year, month, date, hh, mm, ss
 item1 = Item(
         title = "Mighty have the fallen",
         link = f"https://www.{num}.com",
-        description = f"{num}",
+        description = description,
         author = "Vivek Gite",
         guid = Guid(f"https://www.{num}.com"),
         pubDate = datetime.datetime.now())
