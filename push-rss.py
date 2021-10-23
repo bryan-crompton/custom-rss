a9cb57aab6e9e53d5bb2ea63b75f34846294bc4c
Bryan Crompton <bryan.crompton@gmail.com>
2021-10-23 14:19:04 -0400

    $(date)

--- push-rss.py
+++ push-rss.py
@@ -8,7 +8,7 @@ num = random.randint(0, 1000000)
 
 # year, month, date, hh, mm, ss
 item1 = Item(
-        title = "My 10 UNIX Command Line Mistakes",
+        title = "Another one",
         link = "https://www.{num}.com",
         description = f"{num}",
         author = "Vivek Gite",
