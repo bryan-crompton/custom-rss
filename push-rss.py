3eaf7fac29c30a5183cf76d2b76c8808e442f55a
Bryan Crompton <bryan.crompton@gmail.com>
2021-10-23 13:56:02 -0400

    update

--- push-rss.py
+++ push-rss.py
@@ -1,29 +1,60 @@
 import datetime
 from rfeed import *
+import feedparser
+import random
+import os
+
+num = random.randint(0, 1000000)
+
 # year, month, date, hh, mm, ss
 item1 = Item(
         title = "My 10 UNIX Command Line Mistakes",
-        link = "https://www.cyberciti.biz/tips/my-10-unix-command-line-mistakes.html",
-        description = "Here are a few mistakes that I made while working at UNIX/Linux prompt.",
-    author = "Vivek Gite",
-    guid = Guid("https://www.cyberciti.biz/tips/my-10-unix-command-line-mistakes.html"),
-        pubDate = datetime.datetime(2017, 8, 01, 4, 0))
+        link = "https://www.{num}.com",
+        description = f"{num}",
+        author = "Vivek Gite",
+        guid = Guid("https://www.{num}.com"),
+        pubDate = datetime.datetime.now())
+        
+dfmt = "%a, %d %b %Y %H:%M:%S %Z" 
  
-item2 = Item(
-        title = "Top 25 Nginx Web Server Best Security Practices",
-        link = "https://www.cyberciti.biz/tips/linux-unix-bsd-nginx-webserver-security.html",
-        description = "Best Nginx web server hardening and security practice for Linux/Unix sysadmins and developers.",
-    author = "Vivek Gite",
-    guid = Guid("https://www.cyberciti.biz/tips/linux-unix-bsd-nginx-webserver-security.html"),
-        pubDate = datetime.datetime(2017, 8, 01, 4, 2))
- 
- 
+stuff = feedparser.parse("my-reading-list.xml")
+
+LEN = 100
+
+items = [item1]
+
+for entry in stuff.entries:
+
+    link = entry['link']
+    description = entry['description']
+    title = entry['title']
+    guid = Guid(link)
+    date = datetime.datetime.strptime(entry['published'], dfmt)
+
+    item = Item(title=title, link=link, description=description, guid=guid,  pubDate=date)
+    items.append(item)
+
+items.sort(key = lambda x: x.pubDate)
+
+print([item.pubDate for item in items])    
+
+if len(items) >= LEN:
+    items = items[-LEN:]
+
+
 feed = Feed(
         title = "My Reading List",
-        link = "https://www.cyberciti.biz/atom/updated.xml",
-        description = "nixCraft Linux and Unix Sysadmin Blog - Recently updated posts",
+        link = "https://raw.githubusercontent.com/bryan-crompton/custom-rss/main/my-reading-list.xml",
+        description = "My personal reading list feed created from bookmarks",
         language = "en-US",
         lastBuildDate = datetime.datetime.now(),
-        items = [item1, item2])
- 
-print feed.rss()
+        items = items)
+
+
+
+f = open("my-reading-list.xml",'w')
+f.write(feed.rss())
+
+os.system("git add .")
+os.system("git commit -m 'update'")
+os.system("git push")
